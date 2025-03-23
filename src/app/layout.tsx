9b0ecb25c54850bca48eb52a48287779944e21c0
Cxndr <livewellandmakethings@gmail.com>
2025-03-23 18:54:37 +0000

    temp page view transitions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
         `}
       >
         <Header/>
-        <ViewTransition>
+        <ViewTransition name="main">
           {children}
         </ViewTransition>
       </body>
--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -26,7 +26,7 @@ export default function MusicPage() {
                 <iframe 
                   width="80%"
                   src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1803408114&color=%23FFB300&auto_play=false&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
-                  className="min-h-2 rounded-md mt-4 border-2 border-th-neutral-950 shadow-th-md shadow-th-pink-500 hover:scale-105 transition-all duration-300"
+                  className="min-h-2 rounded-md mt-4 border-2 border-th-neutral-950 shadow-th-md shadow-th-pink-500 transition-all duration-300"
                 />
 
               </div>
@@ -35,19 +35,19 @@ export default function MusicPage() {
                 
                 <iframe 
                   width="50%" 
-                  className="ml-32 h-full min-h-2 rounded-md border-2 border-th-neutral-950 shadow-th-md shadow-th-yellow-500 hover:scale-105 transition-all duration-300"
+                  className="ml-32 h-full min-h-2 rounded-md border-2 border-th-neutral-950 shadow-th-md shadow-th-yellow-500 transition-all duration-300"
                   src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1677732198&color=%231089b1&auto_play=false&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
                 />
               
                 <iframe 
                   width="50%" 
-                  className="ml-56 h-full min-h-2 rounded-md border-2 border-th-neutral-950 shadow-th-md shadow-th-blue-500 hover:scale-105 transition-all duration-300"
+                  className="ml-56 h-full min-h-2 rounded-md border-2 border-th-neutral-950 shadow-th-md shadow-th-blue-500 transition-all duration-300"
                   src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1326529426&color=%23ef486f&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
                 />
               
                 <iframe 
                   width="50%"
-                  className="ml-4 h-full min-h-2 rounded-md border-2 border-th-neutral-950 shadow-th-md shadow-th-yellow-500 hover:scale-105 transition-all duration-300"
+                  className="ml-4 h-full min-h-2 rounded-md border-2 border-th-neutral-950 shadow-th-md shadow-th-yellow-500 transition-all duration-300"
                   src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1634045799&color=%231089b1&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
                 />
                 
