324f717eb5f5d0659979bfd7b440047e3eac3a02
cxndr <livewellandmakethings@gmail.com>
2025-04-18 06:35:26 +0100

    music page mobile

--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -11,7 +11,7 @@ export default function MusicPage() {
         <div className="h-full w-desktop max-w-11/12">
           <div className="h-full w-full flex flex-col items-end">
 
-            <div className="w-full mt-6 mb-4 md:mt-16 md:mb-28 flex flex-col md:flex-row gap-8 md:gap-16 flex-grow md:h-full">
+            <div className="w-full mt-8 mb-12 md:mt-16 md:mb-28 flex flex-col md:flex-row gap-8 md:gap-16 flex-grow md:h-full">
               
               <div className="flex flex-col items-center justify-start gap-8 md:gap-16 w-full md:w-1/4 md:h-full">
 
@@ -19,7 +19,7 @@ export default function MusicPage() {
                   <AlbumCovers/>
                 </div>
                 
-                <div className="w-full flex items-center justify-center mt-2 mb-6 md:h-1/3 md:-translate-x-4 z-50">
+                <div className="w-full flex items-center justify-center md:h-1/3 md:-translate-x-4">
                   <MusicHearMore />
                 </div>
 
@@ -29,20 +29,20 @@ export default function MusicPage() {
 
               </div>
 
-              <div className="relative flex flex-col gap-4 items-center justify-start md:justify-between w-full md:w-3/4 md:h-full flex-grow md:pb-4">
+              <div className="relative flex flex-col gap-8 items-center justify-start md:justify-between w-full md:w-3/4 md:h-full flex-grow pb-4">
                 
-                <iframe
+                <iframe 
                   src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1803408114&color=%23FFB300&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&enable_api=true"
-                  className="w-full h-0 flex-grow min-h-0 md:h-auto md:max-h-44 rounded-md border-2 border-th-neutral-950 shadow-th-sm md:shadow-th-md shadow-th-pink-500 transition-all duration-300"
+                  className="w-full flex-grow min-h-0 md:h-auto md:max-h-44 rounded-md border-2 border-th-neutral-950 shadow-th-md shadow-th-pink-500 transition-all duration-300"
                 />
 
-                <iframe
-                    className="w-full h-0 flex-grow min-h-0 md:h-auto md:max-h-44 rounded-md border-2 border-th-neutral-950 shadow-th-sm md:shadow-th-md shadow-th-yellow-500 transition-all duration-300"
+                <iframe 
+                  className="w-full flex-grow min-h-0 md:h-auto md:max-h-44 rounded-md border-2 border-th-neutral-950 shadow-th-md shadow-th-yellow-500 transition-all duration-300"
                   src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1677732198&color=%231089b1&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&enable_api=true"
                 />
 
-                <iframe
-                  className="w-full h-0 flex-grow min-h-0 md:h-auto md:max-h-44 rounded-md border-2 border-th-neutral-950 shadow-th-sm md:shadow-th-md shadow-th-blue-500 transition-all duration-300"
+                <iframe 
+                  className="w-full flex-grow min-h-0 md:h-auto md:max-h-44 rounded-md border-2 border-th-neutral-950 shadow-th-md shadow-th-blue-500 transition-all duration-300"
                   src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1326529426&color=%23ef486f&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&enable_api=true"
                 />
 
--- src/components/MusicPlayer.tsx
+++ src/components/MusicPlayer.tsx
@@ -51,7 +51,11 @@ export default function MusicPlayer() {
   }, []);
 
   return (
-    <div className="relative right-9 flex items-center justify-center h-full w-0 -z-10 -top-0 scale-50">
+    // Mobile: absolute, centered top, scaled up slightly, visible
+    // Desktop: relative, original position/size/scale/z-index
+    <div className="absolute top-4 left-1/2 -translate-x-1/2 scale-75 z-10 \
+                    md:relative md:left-auto md:top-0 md:right-9 md:-translate-x-0 \
+                    md:flex md:items-center md:justify-center md:h-full md:w-0 md:-z-10 md:scale-50">
       <SlothCSS className="absolute rotate-42" musicPlaying={isPlaying}/>
     </div>
   );
