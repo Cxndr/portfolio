dc5961de85ca48f5e614ba0dfa384ba568ab5adc
Cxndr <livewellandmakethings@gmail.com>
2025-04-30 09:35:45 +0100

    disabled image optimization temporarlily

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   experimental: {
     viewTransition: true,
   },
+  images: {
+    unoptimized: true, // disable image optimization temporarily until next month kek
+  }
 };
 
 export default nextConfig;
